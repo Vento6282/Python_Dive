81f6b9b05f493809d9720ef774f7200cc331cf16
AndrevAV <koriolanrus@gmail.com>
2025-01-04 13:51:30 +0300

    Added S_02

---
+++ Seminar/S_02/task_01.py
@@ -0,0 +1,23 @@
+# Создайте несколько переменных разных типов.
+# Проверьте к какому типу относятся созданные переменные. 
+
+a = 1
+print(type(a))
+
+a = 1.1
+print(type(a))
+
+a = 'в'
+print(type(a))
+
+a = 'dd'
+print(type(a))
+
+a = [2,3,4,5]
+print(type(a))
+
+a = True
+print(type(a))
+
+a = {'d','q','h','n'}
+print(type(a))
